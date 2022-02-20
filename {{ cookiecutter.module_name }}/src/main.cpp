342cec447fe03bc62ae141e1a9538336afaffc0e
mohammedfajer <65338839+mohammedfajer@users.noreply.github.com>
2022-02-20 19:47:43 +0000

    #init commit

---
+++ {{ cookiecutter.module_name }}/src/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+#define GLAD_GL_IMPLEMENTATION
+#include <glad/glad.h>
+
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
+#include <cstdio>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+int 
+main()
+{
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
